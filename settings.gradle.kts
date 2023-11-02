b77141e3d2397c88a20bcd6e2e9c913523fbcbc8
bewx77 <112966395+bewx77@users.noreply.github.com>
2023-11-02 10:34:27 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Clone Snapchat App"
+include(":app")
+ 
